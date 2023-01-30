d603dd923ac88cbea3e080e0df08310043e53aac
Victor Yap <vicyap24@gmail.com>
2023-01-30 13:35:10 -0600

    Add test_structure for copying files (#2)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
+import shutil
 
 import pytest
+
+from pterratest.test_structure import copy_terraform_folder_to_temp
 
 
 @pytest.fixture
@@ -12,10 +15,18 @@ def test_dir() -> str:
 @pytest.fixture
 def examples_dir(test_dir) -> str:
     """Returns the path to the top-level examples dir."""
-    return os.path.join(test_dir, os.pardir, "examples")
+    return os.path.abspath(os.path.join(test_dir, os.pardir, "examples"))
 
 
 @pytest.fixture
-def terraform_hello_world_example_dir(examples_dir) -> str:
-    """Returns the path to the `terraform_hello_world` example dir."""
-    return os.path.join(examples_dir, "terraform_hello_world")
+def examples_dir_temp(examples_dir) -> str:
+    """Returns a temp dir path to a copy of the examples dir."""
+    d = copy_terraform_folder_to_temp(examples_dir)
+    yield d
+    shutil.rmtree(d)
+
+
+@pytest.fixture
+def terraform_hello_world_example_dir_temp(examples_dir_temp) -> str:
+    """Returns a temp dir path to a copy of the `terraform_hello_world` example dir."""
+    return os.path.join(examples_dir_temp, "terraform_hello_world")
